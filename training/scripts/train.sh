b4dbccc35377733993bd854b308e886288a40b00
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-26 21:45:55 +0100

    Update train.sh

--- training/scripts/train.sh
+++ training/scripts/train.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=General_Usage
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-80:00:00
 
@@ -38,7 +38,7 @@ working_dir=$main_dir/model
 
 # TensorFlow devices; change this to control the GPUs used by Nematus.
 # It should be a list of GPU identifiers. For example, '1' or '0,1,3'
-devices=0
+devices=0,1,2,3
 
 # Training command that closely follows the 'base' configuration from the
 # paper
@@ -80,7 +80,7 @@ CUDA_VISIBLE_DEVICES=$devices python3 $nematus_home/nematus/train.py \
     --learning_rate 0.0005 \
     --patience 10 \
     --maxlen 200 \
-    --token_batch_size 1000 \
+    --token_batch_size 4000 \
     --valid_source_dataset $data_dir/newsdev2019.bpe.$src \
     --valid_target_dataset $data_dir/newsdev2019.bpe.$trg \
     --valid_token_batch_size 1000 \
